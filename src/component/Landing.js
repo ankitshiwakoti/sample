a7ae19985b6425af8dacfb6cd0b9cbd422884cd9
ankit shiwakoti <shiwakotiankit@gmail.com>
2023-03-30 11:03:31 +0545

    project created

---
+++ src/component/Landing.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Landing = () => {
+  console.log("Test ho ");
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <h2>sfdvbaedsfbearb</h2>
+          <h2>sfdvbaedsfbearb</h2>
+          <h2>sfdvbaedsfbearb</h2>
+          <h2>sfdvbaedsfbearb</h2>
+          <button>dfbsdzfb</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
