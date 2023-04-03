2a8181749fc8a2316a0b36c8c9c17b0f29215140
ankit shiwakoti <shiwakotiankit@gmail.com>
2023-04-03 11:14:19 +0545

    caroudel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Accor from "./component/Accor";
 import Landing from "./component/Landing";
+import Accordion from "./component/Accordion";
 
 function App() {
   return (
     <div className="App">
       {/* <Landing /> */}
-      <Accor />
+      {/* <Accor /> */}
+      <Accordion />
     </div>
   );
 }
--- src/component/Accordion.js
+++ src/component/Accordion.js
@@ -1,170 +1,57 @@
 import React from "react";
+import c1 from "../img/c1.jpg";
+import c3 from "../img/c3.jpg";
 
 const Accordion = () => {
   return (
     <div className="container">
-      <div class="accordion" id="accordionExample">
-        <div class="accordion-item pt-3" style={{ backgroundColor: "#b7baaf" }}>
-          <h2
-            class="accordion-header"
-            id="headingOne"
-            style={{ backgroundColor: "#b7baaf" }}
-          >
-            <button
-              class="accordion-button"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseOne"
-              aria-expanded="true"
-              aria-controls="collapseOne"
-              style={{ color: "black", fontSize: "18px", fontWeight: "bold" }}
-            >
-              {" "}
-              Do I need technical knowledge to use your software product ?
-            </button>
-          </h2>
-          <div
-            id="collapseOne"
-            class="accordion-collapse collapse show"
-            aria-labelledby="headingOne"
-            data-bs-parent="#accordionExample"
-          >
-            <div class="accordion-body font-weight-light">
-              It is shown by default, until the collapse plugin adds the
-              appropriate classes that we use to style each element. These
-              classes control the overall appearance, as well as the showing and
-              hiding via CSS transitions. You can modify any of this with custom
-              CSS or overriding our default variables. It's also worth noting
-              that just about any HTML can go within the , though the transition
-              does limit overflow.
-            </div>
+      <div
+        id="carouselExampleControlsNoTouching"
+        class="carousel slide"
+        data-bs-touch="false"
+        data-bs-interval="false"
+      >
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
+              class="d-block w-100"
+              alt="..."
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/new/slides/046.webp"
+              class="d-block w-100"
+              alt="..."
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/new/slides/045.webp"
+              class="d-block w-100"
+              alt="image"
+            />
           </div>
         </div>
-        <div class="accordion-item pt-3" style={{ backgroundColor: "#b7baaf" }}>
-          <h2 class="accordion-header" id="headingTwo">
-            <button
-              class="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseTwo"
-              aria-expanded="false"
-              aria-controls="collapseTwo"
-              style={{ color: "black", fontSize: "18px", fontWeight: "bold" }}
-            >
-              How secure is your platform ?
-            </button>
-          </h2>
-          <div
-            id="collapseTwo"
-            class="accordion-collapse collapse"
-            aria-labelledby="headingTwo"
-            data-bs-parent="#accordionExample"
-          >
-            <div class="accordion-body font-weight-light">
-              It is shown by default, until the collapse plugin adds the
-              appropriate classes that we use to style each element. These
-              classes control the overall appearance, as well as the showing and
-              hiding via CSS transitions. You can modify any of this with custom
-              CSS or overriding our default variables. It's also worth noting
-              that just about any HTML can go within the , though the transition
-              does limit overflow.
-            </div>
-          </div>
-        </div>
-
-        <div class="accordion-item " style={{ backgroundColor: "#b7baaf" }}>
-          <h2 class="accordion-header" id="headingThree">
-            <button
-              class="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseThree"
-              aria-expanded="false"
-              aria-controls="collapseThree"
-              style={{ color: "black", fontSize: "18px", fontWeight: "bold" }}
-            >
-              What payment method do you accept ?
-            </button>
-          </h2>
-          <div
-            id="collapseThree"
-            class="accordion-collapse collapse"
-            aria-labelledby="headingThree"
-            data-bs-parent="#accordionExample"
-          >
-            <div class="accordion-body font-weight-light">
-              It is shown by default, until the collapse plugin adds the
-              appropriate classes that we use to style each element. These
-              classes control the overall appearance, as well as the showing and
-              hiding via CSS transitions. You can modify any of this with custom
-              CSS or overriding our default variables. It's also worth noting
-              that just about any HTML can go within the , though the transition
-              does limit overflow.
-            </div>
-          </div>
-        </div>
-        <div class="accordion-item" style={{ backgroundColor: "#b7baaf" }}>
-          <h2 class="accordion-header" id="headingTwo">
-            <button
-              class="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseTwo"
-              aria-expanded="false"
-              aria-controls="collapseTwo"
-              style={{ color: "black", fontSize: "18px", fontWeight: "bold" }}
-            >
-              How secure is your platform ?
-            </button>
-          </h2>
-          <div
-            id="collapseTwo"
-            class="accordion-collapse collapse"
-            aria-labelledby="headingTwo"
-            data-bs-parent="#accordionExample"
-          >
-            <div class="accordion-body font-weight-light">
-              It is shown by default, until the collapse plugin adds the
-              appropriate classes that we use to style each element. These
-              classes control the overall appearance, as well as the showing and
-              hiding via CSS transitions. You can modify any of this with custom
-              CSS or overriding our default variables. It's also worth noting
-              that just about any HTML can go within the , though the transition
-              does limit overflow.
-            </div>
-          </div>
-        </div>
-        <div class="accordion-item" style={{ backgroundColor: "#b7baaf" }}>
-          <h2 class="accordion-header" id="headingTwo">
-            <button
-              class="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseTwo"
-              aria-expanded="false"
-              aria-controls="collapseTwo"
-              style={{ color: "black", fontSize: "18px", fontWeight: "bold" }}
-            >
-              How secure is your platform ?
-            </button>
-          </h2>
-          <div
-            id="collapseTwo"
-            class="accordion-collapse collapse"
-            aria-labelledby="headingTwo"
-            data-bs-parent="#accordionExample"
-          >
-            <div class="accordion-body font-weight-light">
-              It is shown by default, until the collapse plugin adds the
-              appropriate classes that we use to style each element. These
-              classes control the overall appearance, as well as the showing and
-              hiding via CSS transitions. You can modify any of this with custom
-              CSS or overriding our default variables. It's also worth noting
-              that just about any HTML can go within the , though the transition
-              does limit overflow.
-            </div>
-          </div>
-        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleControlsNoTouching"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleControlsNoTouching"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
       </div>
     </div>
   );
